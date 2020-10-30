0b33557614d0d83c977ceae7d9dcc886dd35acae
Dimitri Rose <dimitri.ea.rose@gmail.com>
2020-10-30 17:12:05 -0400

    components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const firebase = require('firebase');
+
 class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNot: null,
+      notes: null
+    }
+  }
   
   render() {
     return (
-        <div>qApp</div>
+        <div>App</div>
     );
+  }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes)
+        this.setState({notes: notes});
+      });
   }
 }
 
