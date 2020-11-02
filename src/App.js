94dbd29ddaafb7a85b0f065ad650730ad7a5edb2
Dimitri Rose <dimitri.ea.rose@gmail.com>
2020-11-02 15:42:44 -0500

    couple bugs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import SidebarComponent from './sidebar/sidebar';
+import EditorComponent from './editor/editor';
 import './App.css';
 
 const firebase = require('firebase');
 
 class App extends React.Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
       selectedNoteIndex: null,
-      selectedNot: null,
+      selectedNote: null,
       notes: null
-    }
+    };
   }
-  
+
   render() {
-    return (
-        <div>App</div>
+    return(
+      <div className="app-container">
+        <SidebarComponent 
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}
+          deleteNote={this.deleteNote}
+          selectNote={this.selectNote}
+          newNote={this.newNote}></SidebarComponent>
+        {
+          this.state.selectedNote ?
+          <EditorComponent selectedNote={this.state.selectedNote}
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}
+          noteUpdate={this.noteUpdate}></EditorComponent> :
+          null
+        }
+      </div>
     );
   }
 
@@ -30,9 +47,57 @@ class App extends React.Component {
           data['id'] = _doc.id;
           return data;
         });
-        console.log(notes)
-        this.setState({notes: notes});
+        console.log(notes);
+        this.setState({ notes: notes });
       });
+  }
+
+  selectNote = (note, index) => this.setState({ selectedNoteIndex: index, selectedNote: note });
+  noteUpdate = (id, noteObj) => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .doc(id)
+      .update({
+        title: noteObj.title,
+        body: noteObj.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+  }
+  newNote = async (title) => {
+    const note = {
+      title: title,
+      body: ''
+    };
+    const newFromDB = await firebase
+      .firestore()
+      .collection('notes')
+      .add({
+        title: note.title,
+        body: note.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+    const newID = newFromDB.id;
+    await this.setState({ notes: [...this.state.notes, note] });
+    const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]);
+    this.setState({ selectedNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex });
+  }
+  deleteNote = async (note) => {
+    const noteIndex = this.state.notes.indexOf(note);
+    await this.setState({ notes: this.state.notes.filter(_note => _note !== note) });
+    if(this.state.selectedNoteIndex === noteIndex) {
+      this.setState({ selectedNoteIndex: null, selectedNote: null });
+    } else {
+      this.state.notes.length > 1 ?
+      this.selectNote(this.state.notes[this.state.selectedNoteIndex - 1], this.state.selectedNoteIndex - 1) :
+      this.setState({ selectedNoteIndex: null, selectedNote: null });
+    }
+
+    firebase
+      .firestore()
+      .collection('notes')
+      .doc(note.id)
+      .delete();
   }
 }
 
--- src/editor/editor.js
+++ src/editor/editor.js
@@ -8,13 +8,36 @@ import styles from './styles';
 class EditorComponent extends React.Component {
     constructor() {
         super();
+        this.state = {
+            text: '',
+            title: '',
+            id: ''
+        };
     }
 
     render() {
+
+        const { classes } = this.props;
+       
+       
         return(
-            <div>Editor</div>
+            <div className={classes.editorContainer}>
+                <ReactQuill
+                    value={this.state.text} 
+                    onChange={this.updateBody}>
+                 </ReactQuill>
+                
+            </div>
         )
     }
+    updateBody = async (val) => {
+        await this.setState({ text: val })
+        this.updateBody()
+    }
+    update = debounce (() => {
+        console.log("updating db")
+    }, 1500)
+
 }
 
-export default withStyles(stlyes)(EditorComponent)
+export default withStyles(styles)(EditorComponent)
